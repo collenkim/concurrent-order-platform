fd7d68463148e594b31197a17807f24b5f7c3c3b
collenkim <sasin8712@gmail.com>
2025-06-23 11:41:31 +0900

    초기 프로젝트 생성

---
+++ src/main/java/concurrent/order/platform/ConcurrentOrderPlatformApplication.java
@@ -0,0 +1,13 @@
+package concurrent.order.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentOrderPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentOrderPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/concurrent/order/platform/ConcurrentOrderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package concurrent.order.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentOrderPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
